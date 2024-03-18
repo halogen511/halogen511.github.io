f65ec8f6fe135bc904e0eda6eaa00a18ab6c1dbb
halogen511 <147287251+halogen511@users.noreply.github.com>
2024-03-18 21:30:12 +0900

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,13 @@ const problem = {
     4: '2x - 1 = 7',
     5: '-5x - 3 = 2',
     6: 'x - 11 = -12',
+    7: 'x² - 4x + 4 = 0',
+    8: 'x² + 6x + 9 = 0',
+    9: 'x² + 10x + 25 = 0',
+    10: 'x³ - 9x² + 27x - 27 = 0',
+    11: 'log₉ x = log₃ (x - 2)',
+    12: 'log₃ (x + 2) = 2',
+    13: 'log₃ x + log₃(x - 1) = 12',
 };
 const ans = {
     '-3x - 5 = 1' : '-2',
@@ -13,18 +20,26 @@ const ans = {
     '2x - 1 = 7' : '4',
     '-5x - 3 = 2': '-1',
     'x - 11 = -12': '-1',
+    'x² - 4x + 4 = 0': '2',
+    'x² + 6x + 9 = 0': '-3',
+    'x² + 10x + 25 = 0': '-5',
+    'x³ - 9x² + 27x - 27 = 0': '3',
+    'log₉ x = log₃ (x - 2)': '4',
+    'log₃ (x + 2) = 2': '7',
+    'log₃ x + log₃(x - 1) = 12': '4',
 }
 
 const problems = document.querySelector('.problems');
 const btn = document.querySelector('.ans_button');
 const number = document.querySelector('.number');
-const true_audio = new Audio('./correct1.mp3');
-const wrong_audio = new Audio('./Quiz-Wrong_Buzzer02-1.mp3');
+const true_audio = new Audio('./music/correct1.mp3');
+const wrong_audio = new Audio('./music/Quiz-Wrong_Buzzer02-1.mp3');
 const sleep = waitTime => new Promise( resolve => setTimeout(resolve, waitTime) );
 let cnt = 1;
 
 let len = Object.keys(problem);
 let random_num = Math.floor(Math.random() * len.length  + 1);
+let ans_flag = false;
 
 function add(){
     // 前のイベントリスナーを削除する
@@ -34,7 +49,7 @@ function add(){
     problems.innerHTML = `<h1>${problem[random_num]}</h1>`;
     number.textContent = cnt;
     cnt++;
-    console.log(cnt);
+    ans_flag = false;
 
     document.querySelector('.ans_box').value = '';
 
@@ -47,7 +62,9 @@ async function handleButtonClick() {
     if(ans[problem[random_num]] === text){
         console.log('正解');
         true_audio.play();
+        ans_flag = true;
         await sleep(500);
+        startTimer();
         add();
     }else{
         console.log('不正解');
@@ -56,7 +73,31 @@ async function handleButtonClick() {
     }
 }
 
-btn.addEventListener('click', handleButtonClick);
+function startTimer() {
+    let now_time = 0;
+    const intervalId = setInterval(() => {
+        now_time++;
+        console.log(now_time);
+        if(now_time >= 10){
+            console.log('Time Over');
+            clearInterval(intervalId);
+        }
+        if(ans_flag){
+            console.log('test');
+            clearInterval(intervalId);
+        }
+    }, 1000);
+}
 
-// 初回の問題表示
-add();
+function startGame(){
+    btn.addEventListener('click', handleButtonClick);
+    // 初回の問題表示
+    add();
+    startTimer();
+}
+
+function gameOver(){
+    console.log('game over');
+}
+
+startGame();
